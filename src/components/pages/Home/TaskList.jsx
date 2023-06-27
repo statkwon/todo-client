4575b2727448a1267d8ce7275de46a23c1b7ebf2
statkwon <statkwon96@gmail.com>
2023-06-27 21:58:27 +0900

    [ADD] add a clear button

--- src/components/pages/Home/TaskList.jsx
+++ src/components/pages/Home/TaskList.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Task from 'components/pages/Home/Task.jsx';
 import 'assets/styles/TaskList.scss';
+import icArrowDown from 'assets/images/ic_arrow_down.png';
 
-const TaskList = ({ tasks, updateTask, removeTask }) => {
+const TaskList = ({ tasks, updateTask, removeTask, removeTasks }) => {
+  const clearAllBtnClickHandler = () => removeTasks();
+
   return (
     <div className="tasklist">
+      <div className="tasklist-menu">
+        <div className="tasklist-menu-dropdown">
+          Oldest
+          <img src={icArrowDown} alt="ic_arrow_donw.png" />
+        </div>
+        <button className="tasklist-menu-clear-btn" onClick={clearAllBtnClickHandler}>
+          Clear All
+        </button>
+      </div>
       <div className="tasklist-items">
         {tasks.map(task => (
           <Task key={task.id} task={task} updateTask={updateTask} removeTask={removeTask} />
@@ -19,6 +31,7 @@ TaskList.propTypes = {
   tasks: PropTypes.array,
   updateTask: PropTypes.func,
   removeTask: PropTypes.func,
+  removeTasks: PropTypes.func,
 };
 
 export default TaskList;
